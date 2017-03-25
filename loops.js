3cc08d2241aa82e9a3705237d88847b320552ff6
Nicholas D'Amico <nickalan82@icloud.com>
2017-03-25 21:41:11 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,9 +1,11 @@
+
 function forLoop( array ) {
-  for ( let i = 0; i < 25; i++ ) {
-    let string = i === 0 ? "I am 1 strange loop." : `I am #{i} strange loops.`
+  let string;
+  for( let i = 0; i < 25; i++ ) {
+    string = i === 0 ? "I am 1 strange loop." : `I am #{i} strange loops.`;
     array.push(string);
   }
-  return array;
+  return array
 }
 
 function whileLoop( number ) {
